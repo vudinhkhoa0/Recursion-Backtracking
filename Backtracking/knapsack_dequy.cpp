be1cfd4a9a88d0107aa546a76315c4e208ca6423
vudinhkhoa0 <126996254+vudinhkhoa0@users.noreply.github.com>
2024-03-19 15:10:34 +0700

    U

--- Backtracking/knapsack_dequy.cpp
+++ Backtracking/knapsack_dequy.cpp
@@ -1,7 +1,53 @@
 #include "../../Doan_QLCB/lib/Library.h"
+class Tuple
+{
+    public:
+        int first, second, third;
+        Tuple(int first, int second, int third)
+        {
+            this->first = first;
+            this->second = second;
+            this->third = third;
+        };
+        ~Tuple(){};
+};
+
+Tuple best_sum(-1e9, 0, 0);
+int n, W, *v = new int[n], *w = new int [n];
+
+void Open()
+{
+    cin >> n >> W;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> w[i] >> v[i];
+    }
+}
+
+void Backtracking(int i, Tuple sum)
+{
+    if (i == n) 
+        {
+            if (sum.second <= W)
+            {    
+                if (best_sum.first < sum.first) best_sum = sum;
+                else if (best_sum.first == sum.first && sum.third > best_sum.third) best_sum = sum;
+            }
+            return;
+        }
+    Backtracking(i + 1, {sum.first, sum.second, sum.third});
+    Backtracking(i + 1, {sum.first + v[i], sum.second + w[i], sum.third + 1});
+}
+
+void Print()
+{
+    cout << best_sum.first << " " << best_sum.third;
+}
 
 int main()
 {
-    
+    Open();
+    Backtracking(0, {0, 0, 0});
+    Print();
     return 0;
 }
